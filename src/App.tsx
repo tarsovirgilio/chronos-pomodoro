82ffc49ae01edc718db0f634393440baec2c41f1
Tarso Virgilio <tarsovirgilioc@gmail.com>
2025-07-31 20:47:44 -0300

    feat: first component and learning css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
+import { Heading } from './components/Heading';
+
 import './styles/theme.css';
 import './styles/global.css';
 
@@ -9,25 +10,22 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <Heading />
       <div className='card'>
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+      <p>
+        Bacon ipsum dolor amet picanha prosciutto doner pig sausage capicola,
+        buffalo ribeye pork chop pastrami andouille meatball boudin filet mignon
+        chislic. Pork loin bresaola prosciutto andouille. Porchetta cow
+        landjaeger shankle. Pig tri-tip leberkas, frankfurter pastrami fatback
+        bacon pork meatloaf tenderloin rump shoulder venison. Pork chop
+        frankfurter short loin landjaeger prosciutto, andouille brisket t-bone
+        tongue tri-tip bresaola jerky beef ribs cupim. Strip steak ball tip
+        buffalo flank alcatra, cupim beef ribs prosciutto pork ham hock. Chuck
+        landjaeger filet mignon spare ribs buffalo.
       </p>
     </>
   );
---
+++ src/components/Heading.tsx
@@ -0,0 +1,9 @@
+import styles from './Heading.module.css';
+
+export function Heading() {
+  return (
+    <>
+      <h1 className={styles.header}>Olá mundo com bacon</h1>
+    </>
+  );
+}
