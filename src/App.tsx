772b6cb91223b59a3a51bd53f73bd19ac0a1941e
Tarso Virgilio <tarsovirgilioc@gmail.com>
2025-08-06 21:36:34 -0300

    feat: change page theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { DefaultInput } from './components/DefaultInput';
 
 export default function App() {
   return (
@@ -36,10 +40,16 @@ export default function App() {
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
           <div className='formRow'>
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0 0</p>
+            <Cycles />
+          </div>
+          <div className='formRow'>
+            <DefaultButton icon={<PlayCircleIcon />} color='green' />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,60 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useEffect, useState } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+    setTheme(prevTheme => {
+      const nexTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nexTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
   return (
     <>
       <nav className={styles.menu}>
-        <a className={styles.menuLink} href='#'>
+        <a
+          className={styles.menuLink}
+          href='#'
+          aria-label='Ir para a home'
+          title='Ir para a home'
+        >
           <HouseIcon />
         </a>
-        <a className={styles.menuLink} href='#'>
+        <a
+          className={styles.menuLink}
+          href='#'
+          aria-label='Ver histórico'
+          title='Ver histórico'
+        >
           <HistoryIcon />
         </a>
-        <a className={styles.menuLink} href='#'>
+        <a
+          className={styles.menuLink}
+          href='#'
+          aria-label='Configurações'
+          title='Configurações'
+        >
           <SettingsIcon />
         </a>
-        <a className={styles.menuLink} href='#'>
+        <a
+          className={styles.menuLink}
+          href='#'
+          aria-label='Mudar tema'
+          title='Mudar tema'
+          onClick={handleThemeChange}
+        >
           <SunIcon />
         </a>
       </nav>
